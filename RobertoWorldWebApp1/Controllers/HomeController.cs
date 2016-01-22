474cd3a5aff03f02d984f9a8e02a12059df16b8f
Roberto Carrillo <Roberto.Carrillo@Improving.com>
2016-01-22 09:56:33 -0600

    Clean up

--- RobertoWorldWebApp1/Controllers/HomeController.cs
+++ RobertoWorldWebApp1/Controllers/HomeController.cs
@@ -22,21 +22,7 @@ namespace RobertoWorldWebApp1.Controllers
 
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return RedirectToAction("CalculateSumOfTwoNumbers");
         }
 
         public ActionResult CalculateSumOfTwoNumbers()
