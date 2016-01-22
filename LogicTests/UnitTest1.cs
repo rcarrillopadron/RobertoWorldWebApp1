309c7fbbd65b574045a2247daa4b2252f25b23e8
Roberto Carrillo <Roberto.Carrillo@Improving.com>
2016-01-22 14:18:49 -0600

    Added VS tests

---
+++ LogicTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var service = new CalculateSumOfTwoNumbersService();
+            var request = new SumOfTwoNumbers
+            {
+                Value1 = 345,
+                Value2 = 123
+            };
+            var response = service.Calculate(request);
+
+            Assert.AreEqual(468, response.Result);
+        }
+    }
+}
